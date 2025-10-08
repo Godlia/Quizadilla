4b3465fbff8015b3273c4c8d5caedec4504133aa
dawid-jarz <dawidjarzebiak4@gmail.com>
2025-10-08 15:08:11 +0200

    CLEANING UP CODE

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+    
+// Sidebar toggle logic
+    const sidebar = document.getElementById("sidebar");
+    const menuToggle = document.getElementById("menuToggle");
+    const closeSidebar = document.getElementById("closeSidebar");
+
+    menuToggle.addEventListener("click", () => {
+      sidebar.classList.toggle("active");
+    });
+    closeSidebar.addEventListener("click", () => {
+      sidebar.classList.remove("active");
+    });
