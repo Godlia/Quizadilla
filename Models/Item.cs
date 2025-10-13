1ea33a6eb84012e1a2d8219dea22bd80423b6c60
Godlia <52934844+Godlia@users.noreply.github.com>
2025-10-13 11:53:06 +0200

    backend start

---
+++ Models/Item.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Quizadilla.Models
+{
+    public class Item
+{
+    public int ItemID { get; set; }
+        public string Name { get; set; } = string.Empty;
+    public decimal Price  { get; set; }
+    public string? Description { get; set; }
+    public string? ImageURL { get; set; }
+}    
+}
