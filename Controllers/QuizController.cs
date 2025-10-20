24f46291c339c186209d9c33b815a7cc6befa41b
Eirik H. Kristoffersen <eirik.h.kristoffersen@gmail.com>
2025-10-20 15:20:21 +0200

    CRUD implementation

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -7,6 +7,7 @@ using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 namespace Quizadilla.Controllers;
+
 public class QuizController : Controller
 {
     private readonly QuizDbContext db;
@@ -16,21 +17,171 @@ public class QuizController : Controller
         db = context;
     }
 
-    public IActionResult Quiz(int id = 0)
+    [HttpGet]
+    public IActionResult Edit(int id)
     {
-        Quiz quiz = db.Quizzes.Include(q => q.Questions).FirstOrDefault(q => q.QuizId == id);
+        var quiz = db.Quizzes
+            .Include(q => q.Questions)
+            .ThenInclude(q => q.options)
+            .FirstOrDefault(q => q.QuizId == id);
+
+        if (quiz == null)
+            return NotFound();
+
         return View(quiz);
     }
+
+    [HttpPost]
+    public IActionResult Delete(int id)
+    {
+        var quiz = db.Quizzes
+            .Include(q => q.Questions)
+            .ThenInclude(q => q.options)
+            .FirstOrDefault(q => q.QuizId == id);
+
+        if (quiz == null)
+        {
+            return NotFound();
+        }
+
+        // Remove related entities (Options -> Questions -> Quiz)
+        foreach (var question in quiz.Questions.ToList())
+        {
+            // Remove all options for this question
+            if (question.options != null && question.options.Any())
+            {
+                foreach (var option in question.options.ToList())
+                {
+                    db.Remove(option);
+                }
+            }
+
+            db.Remove(question);
+        }
+
+        db.Remove(quiz);
+        db.SaveChanges();
+
+        return RedirectToAction("Discover");
+    }
+
+
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Quiz updatedQuiz)
+    {
+        if (!ModelState.IsValid)
+            return View(updatedQuiz);
+
+        var existingQuiz = db.Quizzes
+            .Include(q => q.Questions)
+            .ThenInclude(q => q.options)
+            .FirstOrDefault(q => q.QuizId == updatedQuiz.QuizId);
+
+        if (existingQuiz == null)
+            return NotFound();
+
+        // --- Update quiz fields ---
+        existingQuiz.Title = updatedQuiz.Title;
+        existingQuiz.Description = updatedQuiz.Description;
+
+        // --- Handle Questions ---
+        var existingQuestions = existingQuiz.Questions.ToList();
+
+        // Remove deleted questions
+        foreach (var existingQuestion in existingQuestions)
+        {
+            if (!updatedQuiz.Questions.Any(q => q.Id == existingQuestion.Id))
+            {
+                db.Remove(existingQuestion);
+            }
+        }
+
+        // Add or update questions
+        foreach (var q in updatedQuiz.Questions)
+        {
+            var existingQuestion = existingQuestions.FirstOrDefault(eq => eq.Id == q.Id);
+
+            if (existingQuestion == null)
+            {
+                // New question
+                existingQuiz.Questions.Add(q);
+            }
+            else
+            {
+                // Update question
+                existingQuestion.QuestionText = q.QuestionText;
+                existingQuestion.correctString = q.correctString;
+
+                // --- Handle Options ---
+                var existingOptions = existingQuestion.options.ToList();
+
+                // Remove deleted options
+                foreach (var existingOption in existingOptions)
+                {
+                    if (!q.options.Any(o => o.OptionId == existingOption.OptionId))
+                    {
+                        db.Remove(existingOption);
+                    }
+                }
+
+                // Add or update options
+                foreach (var opt in q.options)
+                {
+                    var existingOpt = existingOptions.FirstOrDefault(o => o.OptionId == opt.OptionId);
+                    if (existingOpt == null)
+                    {
+                        existingQuestion.options.Add(opt);
+                    }
+                    else
+                    {
+                        existingOpt.OptionText = opt.OptionText;
+                    }
+                }
+            }
+        }
+
+        db.SaveChanges();
+        return RedirectToAction("Discover");
+    }
+
+
+
+
+    public IActionResult Quiz(int id = 0)
+    {
+        Quiz quiz = db.Quizzes
+            .Include(q => q.Questions)
+            .ThenInclude(q => q.options)
+            .FirstOrDefault(q => q.QuizId == id);
+
+        return View(quiz);
+    }
+
     public IActionResult Index()
     {
         return View();
     }
+    public IActionResult Create(Quiz quiz)
+    {
+        if (ModelState.IsValid)
+        {
+            db.Quizzes.Add(quiz);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return View(quiz);
+    }
+
 
     public IActionResult Discover()
     {
-        
+
         var peepee = new Quiz();
-        peepee.Title = "Test Quiz";
+        peepee.Title = "Fatass";
         peepee.Description = "Just a test quiz.";
         peepee.Questions = new List<Question>
         {
@@ -47,8 +198,8 @@ public class QuizController : Controller
           };
 
 
-        db.Quizzes.Add(peepee);
-        db.SaveChanges();
+        //db.Quizzes.Add(peepee);
+        //db.SaveChanges();
 
         Console.WriteLine("Quizzes added to the database.");
 
@@ -60,6 +211,6 @@ public class QuizController : Controller
             Console.WriteLine(quiz.toString());
         }
 
-        return View();
+        return View(quizzes);
     }
 }
